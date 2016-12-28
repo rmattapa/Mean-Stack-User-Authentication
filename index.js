dfa66a7eb2b358da7626524ba53f792b90f85259
Rajesh <rajeshprince186@gmail.com>
2016-12-28 11:32:58 -0600

    main server file to run the express app

---
+++ index.js
@@ -0,0 +1,39 @@
+
+var app = require('./server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+console.log('Server listening on port 3000!');
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
